68a2e8b1e49e50c1eb94fd197ac558250ea1f483
Marat <almabekov.marat@gmail.com>
2023-06-06 00:45:16 +0600

    Function to animate sun

--- app/src/main/java/com/demoapp/sunset/MainActivity.kt
+++ app/src/main/java/com/demoapp/sunset/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.demoapp.sunset
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AccelerateInterpolator
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +20,18 @@ class MainActivity : AppCompatActivity() {
         sunView = findViewById(R.id.sun)
         skyView = findViewById(R.id.sky)
 
+        sceneView.setOnClickListener { startAnimation() }
+    }
 
+    private fun startAnimation() {
+        val sunYStart = sunView.top.toFloat()
+        val sunYEnd = skyView.height.toFloat()
+
+        val heightAnimator = ObjectAnimator
+            .ofFloat(sunView, "y", sunYStart, sunYEnd)
+            .setDuration(3000)
+        heightAnimator.interpolator = AccelerateInterpolator()
+
+        heightAnimator.start()
     }
 }
