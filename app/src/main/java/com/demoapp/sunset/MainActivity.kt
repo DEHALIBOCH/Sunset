751d6c805b66dd8c1f6da0447e2062cce64f250b
Marat <almabekov.marat@gmail.com>
2023-06-06 00:33:59 +0600

    Preset

--- app/src/main/java/com/demoapp/sunset/MainActivity.kt
+++ app/src/main/java/com/demoapp/sunset/MainActivity.kt
@@ -2,10 +2,22 @@ package com.demoapp.sunset
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sceneView: View
+    private lateinit var sunView: View
+    private lateinit var skyView: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sceneView = findViewById(R.id.scene)
+        sunView = findViewById(R.id.sun)
+        skyView = findViewById(R.id.sky)
+
+
     }
 }
